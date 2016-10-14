5bdb196cde8949a1998570aef792db320a04ea0e
林赟越 <linyunyue@linyunyuedemacbook-pro.local>
2016-10-14 15:33:53 +0800

    初始化项目

---
+++ waterfall/waterfall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waterfall
+//
+//  Created by 林赟越 on 16/10/14.
+//  Copyright © 2016年 林赟越. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ waterfall/waterfall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  waterfall
+//
+//  Created by 林赟越 on 16/10/14.
+//  Copyright © 2016年 林赟越. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
